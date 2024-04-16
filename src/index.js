914b34460b7eb07909b6e48c57578e279453209e
Rafa <rafaokc33@gmail.com>
2024-04-16 20:17:28 +0200

    Shooting bar implementation

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ class Game {
   constructor() {
     this.startScreen = document.getElementById("start-screen");
     this.mainScreen = document.getElementById("main-screen");
-
     this.gameScreen = document.getElementById("game-screen");
-
     this.height = 800;
     this.width = 600;
     this.obstacles = [];
@@ -12,13 +10,20 @@ class Game {
     this.countdown = 30000; // 30 seconds
     this.gameIsOver = false;
     this.gameIntervalId;
-    this.gameLoopFrequency = Math.round(1000 / 60); // 60fps
   }
 
-  start() {
+  openMenu() {
     // Hide the start screen
     this.startScreen.style.display = "none";
 
     // Show the main screen
     this.mainScreen.style.display = "flex";
-}}
+  }
+
+  startGame() {
+    // Occult the main screen
+    this.mainScreen.style.display = "none";
+    // Show the game screen
+    this.gameScreen.style.display = "block";
+  }
+}
--- src/srcript.js
+++ src/srcript.js
@@ -1,12 +1,54 @@
-// change the main screen
-const startScreen = document.getElementById("start-screen");
 
+const startScreen = document.getElementById("start-screen");
+const startButton = document.getElementById("start-button");
+
+// change to the main screen
 window.onload = function () {
   startScreen.addEventListener("click" , function () {
-    showMainSreen();
-})}
+    showMainScreen();
+  })
+  startButton.addEventListener("click" , function () {
+    startGame();
+  }
+)}
 
-function showMainSreen() {
+function showMainScreen() {
   console.log("Open the main screen");
-  new Game().start();
+  new Game().openMenu();
+}
+
+function startGame(){
+  console.log("Starting game");
+  new Game().startGame();
+  barMove()
+}
+
+function barMove() {
+  let myBar = document.getElementById("bar");
+  let width = 1;
+  let fullBar = 0;
+  if (fullBar === 0){
+    const intervalUp = setInterval(function () {
+      if (width === 100) {
+        fullBar = 1;
+        clearInterval(intervalUp);
+      } 
+      else if (fullBar === 0) {
+        width++;
+        myBar.style.width = width + '%';
+        console.log(width);
+      }}, 15);
+  }
+  else {
+    const intervalDown = setInterval(function () {
+      if (width === 0) {
+        fullBar = 0;
+        clearInterval(intervalDown);
+      } 
+      else if (fullBar === 1) {
+      width--;
+      myBar.style.width = width + '%';
+      console.log(width);
+    }}, 15);
+  }
 }
